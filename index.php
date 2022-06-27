634e5eece4c9c4fa3e7e769122be0f39625da009
AbdoulOuadoud <ouadoudbouraima@gmail.com>
2022-06-27 10:31:26 -0700

    home easyFood

---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="assets/logos/logo__easyFood__3.png"/>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Acceuil | easyFood</title>
+</head>
+<body>
+    <header class="header__1 primary__background">
+        <div class = "box__0 header__1__wrapper">
+            <a href="" class="logo">
+                <img src="assets/logos/logo__easyFood__2.png" alt="logo__easyFood">
+            </a>
+            <button class="btn__lg secondary__background" type="button">rejoindre</button>
+        </div>
+    </header>
+
+
+    <main class="main__container">
+
+        <section class="cover">
+
+            <div class="cover__wrapper__1 primary__background">
+                <div class="box__0 cover__wrapper">
+                    <h2 class="light__color">
+                        EasyFood : LE NOUVEAU RESEAU ANTI GASPI en Europe!
+                        Rejoins la communauté Maintenant !!!
+                    </h2>
+                </div>
+            </div>
+
+            <div class="cover__wrapper__2 secondary__background">
+                <div class="circle light__background">
+                    <div class="circlr_wrapper">
+                        <img src="assets/logos/logo__easyFood__1.png" alt="logo__easyFood">
+                    </div>
+                </div>
+                <img src="assets/images/easyFood__img__01.png" alt="">
+                <div class="bottom__logo">
+                    <div class="bottom__logo_wrapper">
+                        <img src="assets/logos/logo__easyFood__1.png" alt="logo__easyFood">
+                    </div>
+                </div>
+                <a href="" class="middle__icon">
+                    <img src="assets/icons/video__icon.png" alt="">
+                </a>
+                <div class="sheet secondary__background"></div>
+            </div>
+
+            <div class="cover__wrapper__3 secondary__background">
+                <div class="box__0 cover__wrapper">
+                    <div class="logo">
+                        <img src="assets/logos/google_play.png" alt="get easyFood on google play">
+                    </div>
+                    <div class="logo">
+                        <img src="assets/logos/app_store.png" alt="dowload easyFood on the app store">
+                    </div>
+                </div>
+            </div>
+        
+        </section>
+
+        <section class="objective primary__background section__container">
+
+            <div class="box__0 objective__wrapper">
+
+                <div class="objective__image">
+                    <img src="assets/images/easyFood__img__02.png" alt="">
+                </div>
+
+                <div class="objetcive__text">
+                    <p class="objetcive__content">
+                        Pour lutter contre le gaspillage 
+                        alimentaire et renforcer l’entraide 
+                        sociale, rejoins la communauté en 
+                        téléchargeant gratuitement 
+                        l’application EasyFood Gether !
+                    </p>
+                </div>
+
+            </div>
+        
+        </section>
+
+        <section class="features light__background section__container">
+
+            <div class="box__0 features_wrapper">
+                <h1 class="section__title secondary__color">nos fonctionalités</h1>
+                <div class="features_wrapper_container">
+                    <div class="card">
+                        <h2 class="card__title primary__color">
+                            Ajout de EasyFood:
+                        </h2>
+
+                        <p class="card__content">
+                            Restaurateurs, agriculteurs et profession-
+                            nels alimentaire: cédez vos plats, fruits, 
+                            produits invendus et minimisez vos pertes 
+                            en les mettant à disposition de millions 
+                            d’utilisateurs;
+                            Particuliers: cédez vos produits non dé-
+                            ballés à travers la grand communauté 
+                            EasyFood.
+                        </p>
+
+                        <div class="card__image">
+                            <img src="assets/images/easyFood__img__05.png" alt="">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h2 class="card__title primary__color">
+                            Retrait de EasyFood:
+                        </h2>
+
+                        <p class="card__content">
+                            Accédez aux EasyFood disponibles près 
+                            de chez vous en souscrivant à un abon-
+                            nement annuel de 24 euros.
+                        </p>
+
+                        <div class="card__image">
+                            <img src="assets/images/easyFood__img__03.png" alt="">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h2 class="card__title primary__color">
+                            ‘’EASYFOOD BONHEUR’’
+                        </h2>
+
+                        <p class="card__content">
+                            A travers cette option vous pouvez faire 
+                            un don de plat, produits, fruits etc. aux 
+                            démunis qui viendrons récupérer 
+                            gratuitement au lieu indiqué.
+                        </p>
+
+                        <div class="card__image">
+                            <img src="assets/images/easyFood__img__04.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="statistics primary__background section__container">
+            <div class="box__0 statistics__wrapper">
+                <div class="statistics__block__1">
+                    <h2 class="light__color section__title statistics__block__title" >statistiques</h2>
+                    <div class="statistics__block__container">
+                        <div class="statistics__block__item light__background">
+                            
+                            <div class="statistic_icon">
+                                <img src="assets/icons/easyFood_added.png" alt="easyFood_added">
+                            </div>
+                            <p class="statistic__block__content secondary__color">5000</p>
+                            <p class="statistic__block__content secondary__color">EasyFood ajoutés</p>
+
+                        </div>
+                        <div class="statistics__block__item light__background">
+                            
+                            <div class="statistic_icon">
+                                <img src="assets/icons/easyFood_removed.png" alt="easyFood_removed">
+                            </div>
+
+                            <p class="statistic__block__content secondary__color">5000</p>
+                            <p class="statistic__block__content secondary__color">EasyFood retirés</p>
+
+                        </div>
+                        <div class="statistics__block__item light__background">
+                            <div class="statistic_icon">
+                                <img src="assets/icons/easyFood_bonheur.png" alt="easyFood_bonheur">
+                            </div>
+                            <p class="statistic__block__content secondary__color">2000</p>
+                            <p class="statistic__block__content secondary__color">EASYFOOD BONHEUR</p>
+                        </div>
+                        <div class="statistics__block__item light__background">
+                            
+                            <div class="statistic_icon">
+                                <img src="assets/icons/easyFood_downloaded.png" alt="easyFood_downloaded">
+                            </div>
+
+                            <p class="statistic__block__content secondary__color">1000</p>
+                            <p class="statistic__block__content secondary__color">Téléchargements</p>
+
+                        </div>
+     
+                    </div>
+                    <div class="statistics_footer">
+                        <a href ="" class="logo">
+                            <img src="assets/logos/google_play.png" alt="get easyFood on google play">
+                        </a>
+                        <a href ="" class="logo">
+                            <img src="assets/logos/app_store.png" alt="dowload easyFood on the app store">
+                        </a>
+                    </div>
+                </div>
+
+                <div class=" statistics__block__2">
+                    <img src="assets/images/easyFood__img__06.png" alt="logo__easyFood">
+                </div>
+
+            </div>
+        
+        </section>
+
+        <section class="partners light__background section__container">
+            <h2 class="partners__title section__title primary__color">
+                nos partenaires
+            </h2>
+
+            <div class="box__0 partners__wrapper">
+                <div class="scroll__arrow">
+                    <span class="icon scroll_arrow_left"></span>
+                </div>
+                <div class="partners__logos">
+                    <div class="partner__logo">
+                        <img src="assets/logos/easy_food_partners_1.png" alt="easy_food_partners">
+                    </div>
+                    <div class="partner__logo">
+                        <img src="assets/logos/easy_food_partners_2.png" alt="easy_food_partners">
+                    </div>
+                    <div class="partner__logo">
+                        <img src="assets/logos/easy_food_partners_3.png" alt="easy_food_partners">
+                    </div>
+                    <div class="partner__logo">
+                        <img src="assets/logos/easy_food_partners_4.png" alt="easy_food_partners">
+                    </div>
+                </div>
+                <div class="scroll__arrow">
+                    <span class="icon scroll_arrow_right"></span>
+                </div>
+            </div>
+        </section >
+
+        <section class="support__project section__container">
+
+            <h2 class="section__title dark__color">
+                soutenir le projet
+            </h2>
+
+            <p class="box__0 support__project__content secondary__color">
+                Donnez le sourire aux plus défavorisés 
+                en faisant un don à partir de 1 euro!
+            </p>
+
+            <a href="" class="make__donation secondary__background">
+                Faire un don
+            </a>
+        
+        </section>
+    
+    </main>
+
+    <footer class="footer">
+        <div class="footer__part__1 primary__background">
+            <div class="box__0 footer__part__1__wrapper">
+                <h2 class="footer__part__1__title light__color">Téléchargez gratuitement EasyFood Gether </h2>
+                <div class="footer__part__1__logo">
+                    <a href ="" class="logo">
+                        <img src="assets/logos/google_play.png" alt="get easyFood on google play">
+                    </a>
+                    <a href ="" class="logo">
+                        <img src="assets/logos/app_store.png" alt="dowload easyFood on the app store">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="footer__part__2 secondary__background">
+
+            <div class="footer__part__2__logo">
+                <img src="assets/logos/logo__easyFood__2.png" alt="logo__easyFood">
+            </div>
+
+            <div class="naviguation">
+                <a href="" class="nav__link light__color">Conditions générales d'utilisation</a>
+                <a href="" class="nav__link light__color">Newsletters</a>
+                <a href="" class="nav__link light__color">Contact</a>
+                <a href="" class="nav__link light__color">FAQs</a>
+            </div>
+
+            <div class="socials_media">
+                <a href="">
+                    <span class="icon__sm social_media_facebook"></span>
+                </a>
+                <a href="">
+                    <span class="icon__sm social_media_instagram"></span>
+                </a>
+                <a href="">
+                    <span class="icon__sm social_media_linkedin"></span>
+                </a>
+                <a href="">
+                    <span class="icon__sm social_media_twitter"></span>
+                </a>
+            </div>
+
+        </div>
+    </footer>
+</body>
+</html>
