688f3e1ca3acf1fb18690272d0dc09932eb33978
ouadoudbouraima <ouadoudbouraima@gmail.com>
2022-07-30 03:13:19 -0700

    correction regis 02

--- js/scroll_script.js
+++ js/scroll_script.js
@@ -3,6 +3,13 @@ const scroll__right = document.getElementById("scroll__right");
 
 const container__to__scroll = document.querySelector('.partners__logos__container');
 
+var slider = setInterval(() => {
+    container__to__scroll.scrollLeft += 150;
+    if (container__to__scroll.scrollLeft >= 400){
+        container__to__scroll.scrollLeft = 0;
+    }
+}, 1000);
+
 scroll__left.addEventListener('click',()=>{
     container__to__scroll.scrollLeft -= 150;
     console.log("dood")
