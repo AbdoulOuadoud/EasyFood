43301a0b5cc0b5ca6d459e33333b59f820977cf6
ouadoudbouraima <ouadoudbouraima@gmail.com>
2022-08-01 03:57:12 -0700

    faire un don

---
+++ js/fud__script.js
@@ -0,0 +1,49 @@
+const step__item = document.getElementsByClassName("step__item")
+const step__btn = document.getElementsByClassName("step__btn");
+const step__content = document.getElementsByClassName("step__content");
+
+for (let index = 0; index < step__btn.length; index++) {
+    const element = step__btn[index];
+    element.addEventListener('click',()=>{
+        try{
+            step__content[index - 1].classList.remove('active__step')
+            step__item[index - 1].classList.remove('doing')
+            step__item[index - 1].classList.add('done')
+        } catch(e){
+            console.log(e)
+        }
+        step__content[index].classList.add('active__step')
+        step__item[index].classList.remove('to__do')
+        step__item[index].classList.add('doing')
+        for (let j = index + 1; j < step__btn.length; j++) {
+            step__item[j].classList.remove('doing')
+            step__item[j].classList.remove('done')
+            step__item[j].classList.add('to__do')
+            step__content[j].classList.remove('active__step')
+
+        }
+    })
+}
+
+const countries = document.getElementById('countries');
+const flags = document.querySelector(".flags");
+
+let previous__flag = ""
+
+countries.addEventListener('change', (event) =>{
+    selected__flag = "fi-"+event.target.value.toLowerCase()
+    if(flags.classList.contains("fi-fr")){
+        flags.classList.replace("fi-fr",selected__flag)
+    } else {
+        flags.classList.replace(previous__flag,selected__flag)
+    }
+    previous__flag = selected__flag;
+})
+
+const check = document.getElementById("check__2")
+const raison__sociale = document.querySelector(".raison__sociale")
+
+check.addEventListener('click', ()=>{
+    raison__sociale.classList.toggle('active')
+})
+
